b976034048e1ce519ad2efaeec06efa3d78e7e86
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-02-02 18:37:15 +0700

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,42 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function Header() {
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStorageChange = () => {
+            setToken(localStorage.getItem('token'));
+        };
+
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        };
+    }, []);
+
+    const handleLogout = async () => {
+        setLoading(true);
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/logout', {
+                method: 'get',
+                headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`},
+            });
+
+            if (response.ok) {
+                localStorage.removeItem('token');
+                setToken(null);
+            }
+        } catch (err) {
+            console.error('Ошибка', err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -16,13 +52,15 @@ export default function Header() {
                                 <Link to="/" className="nav-link">Главная</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/register" className="nav-link">Регистрация</Link>
+                                <Link to="/register" className={`nav-link ${token ? 'd-none' : ''}`}>Регистрация</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/login" className="nav-link">Авторизация</Link>
+                                <Link to="/login" className={`nav-link ${token ? 'd-none' : ''}`}>Авторизация</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/logout" className="nav-link">Выход</Link>
+                                <Link to="/login" className={`nav-link ${!token ? 'd-none' : ''}`}
+                                      onClick={handleLogout}
+                                      aria-disabled={loading}>{loading ? 'Выходим...' : 'Выйти'}</Link>
                             </li>
                         </ul>
                     </div>
